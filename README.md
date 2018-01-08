d7685a3efa4ad4915fe9a18b07def51458bbdae8
wp360 <2897406527@qq.com>
2018-01-08 13:47:34 +0800

    add file

---
+++ README.md
@@ -0,0 +1,3 @@
+## jQuery Plugin
+[官网参考链接:](http://learn.jquery.com/plugins/basic-plugin-creation/)
+[JQuery Plugin 开发](http://www.cnblogs.com/wang_yb/archive/2014/11/17/4103137.html)
--- accordion.js
+++ accordion.js
@@ -1,26 +1,54 @@
-(function($){
-    $.fn.accordion = function(options){
-        var settings = $.extend({},$.fn.accordion.defaults,options);
-        this.find("h3")
-            .addClass("accordion-header")
-            .next()
-            .addClass("accordion-panel")
-            .slideUp();
-        var $this = this;
-        this.on("click",".accordion-header",function(e){
-            var next = $(e.target).next();
-            var isActive = next.attr("data-active");
-            if(settings.distinct){
-                $this.find("[data-active]")
-                    .removeAttr("data-active")
+(function ($) {
+    var methods = {
+        init: function (options) {
+            var settings = $.extend({}, $.fn.accordion.defaults, options);
+            this
+                .find("h3")
+                .addClass("accordion-header")
+                .next()
+                .addClass("accordion-panel")
+                .slideUp();
+            var $this = this;
+            this.on("click", ".accordion-header", function (e) {
+                var next = $(e.target).next();
+                var isActive = next.attr("data-active");
+                if (settings.distinct) {
+                    $this
+                        .find("[data-active]")
+                        .removeAttr("data-active")
+                        .slideToggle();
+                    if(isActive){
+                        return;
+                    }
+                }
+                next
+                    .attr("data-active", true)
                     .slideToggle();
-            }
-            next.attr("data-active",true)
-                .slideToggle();
-        });
-        return this;
+            });
+            return this;
+        },
+        expand: function(){
+            this.find(".accordion-panel")
+                .attr("data-active", true)
+                .slideDown();
+            return this;
+        },
+        collapse: function(){
+            this.find("[data-active]")
+                .removeAttr("data-active")
+                .slideUp();
+            return this;
+        }
     };
+
+    $.fn.accordion = function(options){
+        if(methods[options]){
+            return methods[options].call(this);
+        }
+        return methods.init.call(this,options);
+    };
+
     $.fn.accordion.defaults = {
-        distinct: false
+        distinct: true
     };
 }(jQuery));
