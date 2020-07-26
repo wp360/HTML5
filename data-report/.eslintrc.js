ede032ab471ef05c5b4cb157314cc2bbbec15222
wp360 <2897406527@qq.com>
2020-07-27 00:52:03 +0800

    累计订单量

--- data-report/.eslintrc.js
+++ data-report/.eslintrc.js
@@ -15,5 +15,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     // 取消缩进验证
     // 'indent': 'off'
+    // 函数定义前的空格
+    // 'space-before-function-paren': 'off'
   }
 }
