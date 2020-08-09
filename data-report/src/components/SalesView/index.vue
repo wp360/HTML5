8f631398ce108836cf4410caff3c6ef89851d178
wp360 <2897406527@qq.com>
2020-08-10 01:01:53 +0800

    销售数据表

--- data-report/src/components/SalesView/index.vue
+++ data-report/src/components/SalesView/index.vue
@@ -1,13 +1,14 @@
 <template>
   <!-- 销售数据表 -->
   <div class="sales-view">
-    <el-card shadow="hover">
+    <el-card shadow="hover" :body-style="{padding: '0 0 20px 0'}">
       <template v-slot:header>
         <div class="menu-wrapper">
           <el-menu
             :default-active="activeIndex"
             mode="horizontal"
             @select="onMenuSelect"
+            class="sales-view-menu"
           >
             <el-menu-item index="1">销售额</el-menu-item>
             <el-menu-item index="2">访问量</el-menu-item>
@@ -27,8 +28,34 @@
               start-placeholder="开始时间"
               end-placeholder="结束时间"
               size="small"
+              unlink-panels
               :picker-options="pickerOptions"
+              class="sales-view-date-picker"
             />
+          </div>
+        </div>
+      </template>
+      <template>
+        <div class="sales-view-chart-wrapper">
+          <!-- 左侧图表 -->
+          <v-chart :options="chartOption" />
+          <!-- 右侧列表 -->
+          <div class="sales-view-list">
+            <div class="sales-view-title">排行榜</div>
+            <div class="list-item-wrapper">
+              <div class="list-item" v-for="item in rankData" :key="item.no">
+                <!-- <div class="list-item-no" :class="+item.no <= 3 ? 'top-no' : ''"> -->
+                <div :class="['list-item-no', +item.no <= 3 ? 'top-no' : '']">
+                  {{item.no}}
+                </div>
+                <div class="list-item-name">
+                  {{item.name}}
+                </div>
+                <div class="list-item-money">
+                  {{item.money}}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </template>
@@ -57,16 +84,109 @@ export default {
         {
           text: '最近一个月',
           onClick (picker) {
-
+            const start = new Date()
+            const end = new Date()
+            start.setTime(start.getTime() - 3600 * 24 * 1000 * 30)
+            picker.$emit('pick', [start, end])
           }
         },
         {
           text: '最近三个月',
           onClick (picker) {
-
+            const start = new Date()
+            const end = new Date()
+            start.setTime(start.getTime() - 3600 * 24 * 1000 * 90)
+            picker.$emit('pick', [start, end])
           }
         }]
-      }
+      },
+      chartOption: {
+        title: {
+          text: '年度销售额',
+          textStyle: {
+            fontSize: 12,
+            color: '#666'
+          },
+          left: 25,
+          top: 20
+        },
+        xAxis: {
+          type: 'category',
+          data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+          axisTick: {
+            alignWithLabel: true,
+            lineStyle: {
+              color: '#999'
+            }
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#999'
+            }
+          },
+          axisLabel: {
+            color: '#333'
+          }
+        },
+        yAxis: {
+          axisLine: {
+            show: false
+          },
+          axisTick: {
+            show: false
+          },
+          splitLine: {
+            lineStyle: {
+              type: 'dotted',
+              color: '#eee'
+            }
+          }
+        },
+        series: [{
+          type: 'bar',
+          barWidth: '35%',
+          data: [200, 260, 300, 320, 350, 280, 300, 320, 350, 280, 300, 350]
+        }],
+        color: ['#3398DB'],
+        grid: {
+          top: 70,
+          left: 60,
+          right: 60,
+          bottom: 50
+        }
+      },
+      rankData: [
+        {
+          no: 1,
+          name: '麦当劳',
+          money: '323, 234'
+        },
+        {
+          no: 2,
+          name: '肯德基',
+          money: '523, 234'
+        },
+        {
+          no: 3,
+          name: '必胜客',
+          money: '223, 234'
+        },
+        {
+          no: 4,
+          name: '星巴克',
+          money: '283, 234'
+        },
+        {
+          no: 5,
+          name: '汉堡王',
+          money: '223, 234'
+        },
+        {
+          no: 6,
+          name: '永和大王',
+          money: '203, 000'
+        }
+      ]
     }
   },
   methods: {
@@ -81,8 +201,92 @@ export default {
 <style lang="scss" scoped>
   .sales-view {
     margin-top: 20px;
+
     .menu-wrapper {
+      position: relative;
       display: flex;
+      .sales-view-menu {
+        width: 100%;
+        padding-left: 20px;
+        .el-menu-item {
+          height: 50px;
+          line-height: 50px;
+          margin: 0 20px;
+        }
+      }
+      .menu-right {
+        position: absolute;
+        top: 0;
+        right: 20px;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        .sales-view-date-picker {
+          margin-left: 20px;
+        }
+      }
+    }
+
+    .sales-view-chart-wrapper {
+      display: flex;
+      height: 270px;
+
+      .echarts {
+        flex: 0 0 70%;
+        width: 70%;
+        height: 100%;
+      }
+
+      .sales-view-list {
+        flex: 1;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+
+        .sales-view-title {
+          margin-top: 20px;
+          font-size: 12px;
+          color: #666;
+          font-weight: 500;
+        }
+
+        .list-item-wrapper {
+          margin-top: 15px;
+          .list-item {
+            display: flex;
+            align-items: center;
+            font-size: 12px;
+            height: 20px;
+            padding: 6px 20px 6px 0;
+
+            .list-item-no {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              width: 20px;
+              height: 20px;
+              color: #333;
+              &.top-no {
+                background: #000;
+                border-radius: 50%;
+                color: #fff;
+                font-weight: 500;
+              }
+            }
+
+            .list-item-name {
+              margin-left: 10px;
+              color: #333;
+            }
+
+            .list-item-money {
+              flex: 1;
+              text-align: right;
+            }
+          }
+        }
+      }
     }
   }
 </style>
